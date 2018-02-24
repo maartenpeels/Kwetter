8c71bdd2597dea8a826a82534bba9b4bd79acdb1
Maarten <I292406>
2018-02-24 09:21:35 +0100

    Tweet model

--- src/main/kotlin/com/kwetter/models/Tweet.kt
+++ src/main/kotlin/com/kwetter/models/Tweet.kt
@@ -10,12 +10,12 @@ data class Tweet(
         @Size(max = 140)
         var text: String,
 
-        @ManyToOne @JoinColumn(name = "user_id")
-        var owner: User,
-
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         override var id: Long = -1,
 
         override var createdAt: Date = Date(),
         override var updatedAt: Date = Date()
-) : IModel
+) : IModel {
+    @ManyToOne @JoinColumn(name = "user_id")
+    lateinit var owner: User
+}
