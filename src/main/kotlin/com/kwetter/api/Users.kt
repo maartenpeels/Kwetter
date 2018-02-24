68d3833bc28878bbdf7d7b66d0591e8f3511afb8
Maarten <I292406>
2018-02-24 09:22:10 +0100

    Users endpoint

---
+++ src/main/kotlin/com/kwetter/api/Users.kt
@@ -0,0 +1,28 @@
+package com.kwetter.api
+
+import com.kwetter.services.UserService
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import com.kwetter.models.User
+import javax.ejb.Stateless
+import javax.inject.Named
+import javax.ws.rs.PathParam
+
+@Stateless
+@Named
+@Path("user")
+open class Users @Inject constructor(private var userService: UserService){
+
+    @GET
+    @Path("{id}")
+    fun userById(@PathParam("id") id: Int): User {
+        return userService.getById(id)
+    }
+
+    @GET
+    @Path("test")
+    fun test(): String {
+        return "Werkt"
+    }
+}
